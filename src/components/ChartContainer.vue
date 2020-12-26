0183032b8ef701337ea5752942bb7175030e396d
kkrsky <kurebayashi.ryohei.15@shizuoka.ac.jp>
2020-12-26 14:38:25 +0900

    commit test3

--- src/components/ChartContainer.vue
+++ src/components/ChartContainer.vue
@@ -215,7 +215,7 @@ export default {
       element.remove();
     },
     //APIs
-    //tes23
+    //tes233
     createChartVI({ chartName, labelName, setDataArry }) {
       //   console.log(chartName, labelName)
       let chartVI_ctx = window.document
