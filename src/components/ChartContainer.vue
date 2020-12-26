6aa3d39d5006666a31c930e7ad8bb91a47de17c7
kkrsky <49971230+kkrsky@users.noreply.github.com>
2020-12-26 14:47:44 +0900

    commit test6

--- src/components/ChartContainer.vue
+++ src/components/ChartContainer.vue
@@ -215,7 +215,7 @@ export default {
       element.remove();
     },
     //APIs
-    //tes2334
+    //tes233456
     createChartVI({ chartName, labelName, setDataArry }) {
       //   console.log(chartName, labelName)
       let chartVI_ctx = window.document
